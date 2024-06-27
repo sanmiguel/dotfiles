a335a14848323226ed640a6b404be6eb1d31e114
Michael Coles <michael.coles@gmail.com>
2024-06-27 16:31:06 +0200

    Remove unused plugins

--- nvim.lua/lua/plugins/external.lua
+++ nvim.lua/lua/plugins/external.lua
@@ -5,18 +5,18 @@ return {
   "tpope/vim-rhubarb", -- github support
   "airblade/vim-gitgutter",
   "whiteinge/diffconflicts",
-  {
-	  "ldelossa/gh.nvim",
-	  dependencies = {
-		  {
-		  "ldelossa/litee.nvim",
-		  config = function()
-			  require("litee.lib").setup()
-		  end,
-		  },
-	  },
-	  config = function()
-		  require("litee.gh").setup()
-	  end,
-  }
+  -- {
+	  -- "ldelossa/gh.nvim",
+	  -- dependencies = {
+		  -- {
+		  -- "ldelossa/litee.nvim",
+		  -- config = function()
+			  -- require("litee.lib").setup()
+		  -- end,
+		  -- },
+	  -- },
+	  -- config = function()
+		  -- require("litee.gh").setup()
+	  -- end,
+  -- }
 }
--- nvim.lua/lua/plugins/telescope.lua
+++ nvim.lua/lua/plugins/telescope.lua
@@ -4,16 +4,16 @@ return {
 		-- or                          , branch = '0.1.x',
 		dependencies = { 'nvim-lua/plenary.nvim' }
 	},
-	{
-	  "ahmedkhalf/project.nvim",
-	  config = function()
-		require("project_nvim").setup {
-		  -- your configuration comes here
-		  -- or leave it empty to use the default settings
-		  -- refer to the configuration section below
-		}
-	  end
-	},
+	-- {
+	--   "ahmedkhalf/project.nvim",
+	--   config = function()
+	-- 	require("project_nvim").setup {
+	-- 	  -- your configuration comes here
+	-- 	  -- or leave it empty to use the default settings
+	-- 	  -- refer to the configuration section below
+	-- 	}
+	--   end
+	-- },
 	{
 		"nvim-telescope/telescope-ui-select.nvim",
 		config = function()
