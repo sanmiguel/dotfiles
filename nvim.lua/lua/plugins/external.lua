e0e37823a2dc378f208e554cc71abae840db0bcc
Michael Coles <michael@coles.software>
2026-04-16 13:24:41 +0200

    chore(nvim): remove obsidian.nvim

--- nvim.lua/lua/plugins/external.lua
+++ nvim.lua/lua/plugins/external.lua
@@ -26,38 +26,38 @@ return {
 		  require("litee.gh").setup()
 	  end,
   },
-  {
-	  "epwalsh/obsidian.nvim",
-	  version = "*",
-	  -- lazy = true,
-	  -- ft = "markdown",
-	  -- event = {
-		-- "BufReadPre " .. vim.fn.expand "~" .. "/Obsidian/*.md",
-		-- "BufNewFile " .. vim.fn.expand "~" .. "/Obsidian/*.md"
+  -- {
+	  -- "epwalsh/obsidian.nvim",
+	  -- version = "*",
+	  -- -- lazy = true,
+	  -- -- ft = "markdown",
+	  -- -- event = {
+		-- -- "BufReadPre " .. vim.fn.expand "~" .. "/Obsidian/*.md",
+		-- -- "BufNewFile " .. vim.fn.expand "~" .. "/Obsidian/*.md"
+	  -- -- },
+	  -- dependencies = {
+		-- "nvim-lua/plenary.nvim",
+		-- "hrsh7th/nvim-cmp",
+		-- "nvim-treesitter/nvim-treesitter",
 	  -- },
-	  dependencies = {
-		"nvim-lua/plenary.nvim",
-		"hrsh7th/nvim-cmp",
-		"nvim-treesitter/nvim-treesitter",
-	  },
-	  config = function()
-		  vim.o.conceallevel = 1
-		  require("obsidian").setup({
-			completion = {
-			  nvim_cmp = true,
-			  min_chars = 2
-			},
-			workspaces = {
-			  {
-				name = "Personal",
-				path = "~/Obsidian/Personal"
-			  },
-			},
-			picker = {
-			  name = "telescope.nvim",
-			},
-		  })
-	  end,
-  }
+	  -- config = function()
+		  -- vim.o.conceallevel = 1
+		  -- require("obsidian").setup({
+			-- completion = {
+			  -- nvim_cmp = true,
+			  -- min_chars = 2
+			-- },
+			-- workspaces = {
+			  -- -- {
+				-- -- name = "Personal",
+				-- -- path = "~/Obsidian/Personal"
+			  -- -- },
+			-- },
+			-- picker = {
+			  -- name = "telescope.nvim",
+			-- },
+		  -- })
+	  -- end,
+  -- }
 
 }
